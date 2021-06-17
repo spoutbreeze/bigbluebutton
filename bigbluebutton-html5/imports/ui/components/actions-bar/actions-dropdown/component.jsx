04e419404ad64b41d4fb1cfab5ef50e02ebff222
Himanshu Goyal <himanshugoyal1065@gmail.com>
2021-06-17 18:11:34 +0530

    syncing

--- bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
@@ -93,7 +93,7 @@ class ActionsDropdown extends PureComponent {
     this.pollId = _.uniqueId('action-item-');
     this.takePresenterId = _.uniqueId('action-item-');
     this.selectUserRandId = _.uniqueId('action-item-');
-
+    this.broadcastVideo = _.uniqueId('action-item-');
     this.handleExternalVideoClick = this.handleExternalVideoClick.bind(this);
     this.makePresentationItems = this.makePresentationItems.bind(this);
   }
@@ -215,6 +215,7 @@ class ActionsDropdown extends PureComponent {
             icon = "video"
             label={"Broadcast video"}
             description={"click to broadcast the video"}
+            key={this.broadcastVideo}
             onClick = {() => mountModal(<BroadcastVideo />)}
           />
         ) : null)
